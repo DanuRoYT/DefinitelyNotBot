cc4ce1a36247b574f9075d1ad5765b008443652c
DanuRoYT <42582303+DanuRoYT@users.noreply.github.com>
2018-08-21 21:13:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,15 +7,33 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '??help') {
-    	message.channel.send('` ??programator `= **iti arata programatorul**');
-  	}
+    	message.channel.send('``` 
+                              ??programator = **iti arata programatorul**
+
+                              ??palma @membru = **îi tragi o palmă unui membru**
+                             
+                              ??avatar = **avatarul tau**
+                              ```'}
 });
 
 client.on('message', message => {
     if (message.content === '??programator') {
-    	message.reply('programatorul este DanuRo YT , are 10 ani, este youtuber, este gamei etc. youtube = DanuRo YT');
+    	message.reply('programatorul este DanuRo YT , are 10 ani, este youtuber, este gamer etc. youtube = DanuRo YT');
   	}
 });
+
+client.on('message', message => {
+     
+    if (message.content === '??palma {user}') {
+        message.reply('iai dat o palmă lui {user}');
+    }
+});
+
+ client.on ('message', message => {
+    if ('message.content === '??avatar') {
+         message.reply ('{user.avatar_url}');
+    }
+ });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
