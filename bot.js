9463ac801c4ca74e2bd9bc05cb2c84204124fb4e
DanuRoYT <42582303+DanuRoYT@users.noreply.github.com>
2018-08-21 19:50:03 +0300

    Update bot.js
    Noonvcbkjnsgfokngsjfonl

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === '??help') {
+    	message.channel.send('` ??programator `= **iti arata programatorul');
   	}
 });
 
