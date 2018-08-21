a207ba1fcce5a912b9275287598ca1a143f49728
DanuRoYT <42582303+DanuRoYT@users.noreply.github.com>
2018-08-21 20:45:46 +0300

    Update bot.js
    Noonvcbkjnsgfokngsjfonl

--- bot.js
+++ bot.js
@@ -1,21 +1,30 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+import discord
+from discord.txt import commands
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+bot - commands.Bot (command_prefix-'get ')
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
+@bot_event
+async def on_ready():
+    print("Bot Is Starting Up... Please Wait")
+    print("Name: " + bot.user.name)
+    print("ID: " + bot.user.id)
+    print("Bot Is Online! And Ready To Spam")
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+@bot.command(pass_context=True)
+async def spammed(ctx);
+    await bot.say("Salut toti")
+    time.sleep(2)
+    await bot.say("va fi spam de boi!!!!")
+    for i in range (0.1000000000000000000000000000000000000000000000000000000000000000000000000000):
+        await bot.say(spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm);
+        await bot.say(spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm);
+        await bot.say(spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm);
+        await bot.say(spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm);
+        await bot.say(spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm);
+        await bot.say(spaaaaaaaaaaaaaaaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm);
+        await bot.say(Atât a fost spamat de @DanuRo YT#6977)
+                      
+                      
+                      
+                      
+TOKEN=(BOT_TOKEN)
