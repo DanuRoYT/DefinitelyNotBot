79960d147a9254a24d0c4e62ab2e9b92ecc27331
DanuRoYT <42582303+DanuRoYT@users.noreply.github.com>
2018-08-22 10:56:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,20 @@
-const Discord = require('discord.js');
+const Discord = require("Discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+var prefix = '_'
+var token = 'BOT_TOKEN'
 
-client.on('message', message => {
-    if (message.content === 'helpbot') {
-    	message.channel.send('**programator = arata programatorul botului || avatar = arata avatarul programatorului**');
-  	}
-});
+client.on(ready., function() {
+console.log("ready")
+})
 
-client.on('message', message => {
-    if (message.content === 'programator') {
-    	message.reply('**Programatorul serverului este DanuRo YT#6977,pe youtube se numeste DanuRo YT, este si gamer');
-  	}
-});
+client.on("message", message => {
+	async def programator():
+    embed = discord.Embed(
+        title = 'Utilizatorul : DanuRo YT',
+        description = 'Calm (in unele situatii), trist, in depresie, timid, nu e bagat in seama.',
+        colour = discord.Colour.blue()
+	
+})
 
-client.on('message', message => {
-    if (message.content === 'avatar') {
-    	message.reply(
-    embed.set_image(url='https://cdn.discordapp.com/attachments/453830411009654785/481585979094728709/1534762105947.jpeg');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(BOT_TOKEN);
