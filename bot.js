8352a598913b1189411b2d51293ec77a55dafd29
DanuRoYT <42582303+DanuRoYT@users.noreply.github.com>
2018-08-22 11:12:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,30 @@
-import discord
-from discord.ext import commands
+const Discord = require('discord.js');
+const client = new Discord.client();
 
-TOKEN='BOT_TOKEN'
-client = commands.Bot(command_prefix='??')
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
-@client.event
-async def on_ready():
-    print('gata')
-    print(client.user.name)
+client.on('message', message => {
+    if (message.content === '??help') {
+    message.reply('`??programator` = **iti arata programatorul botului**');
+  	}
+});
 
-@client.command(pass_context=True)
-async def clear(ctx, amount=100000000000000000000000000000000000000000000000):
-    channel = ctx.message.channel
-    messages = []
-    async for message in client.logs_from(channel, limit=int(amount) + 1):
-        messages.append(message)
-    await client.delete_messages(messages)
-    await client.say('Mesajele au fost sterse!', delete_after=1)
+client.on("message", message => {
+	async def programator():
+    embed = discord.Embed(
+        title = 'Utilizatorul : DanuRo YT',
+        description = 'Calm (in unele situatii), trist, in depresie, timid, nu e bagat in seama.',
+        colour = discord.Colour.blue()
+	
+})
 
-client.run(BOT_TOKEN)
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
